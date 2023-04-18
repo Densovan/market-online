e0807527b8ff1e31613928c13f74e24ba4c2a9cf
Densovan <sarimramata@gmail.com>
2023-04-18 22:22:28 +0700

    add category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -52,3 +52,51 @@ exports.update_category = async (req, res) => {
     return res.status(500).json({ msg: error.message });
   }
 };
+
+// @desc delete Category
+// @route DELETE /category
+// @access Private admin
+exports.delete_category = async (req, res) => {
+  try {
+    const category_id = await Category.findById(req.params.id);
+    if (!category_id)
+      return res.stats(401).json({ msg: "invalid category id" });
+    await Category.findByIdAndDelete({ _id: req.params.id });
+    return res.status(200).json({ msg: "category delete successfully" });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+// @desc query Category
+// @route GET /category
+// @access public
+exports.get_category = async (req, res) => {
+  try {
+    const categoryId = await Category.findById(req.params.id);
+    if (!categoryId) return res.stats(401).json({ msg: "invalid category id" });
+    const category = await Category.findById({ _id: req.params.id });
+    return res.status(200).json({ category });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+// @desc query Categories
+// @route GET /categories
+// @access public
+exports.get_categories = async (req, res) => {
+  const page = parseInt(req.query.page) - 1 || 0;
+  const limit = parseInt(req.query.limit) || 5;
+  try {
+    const categories = await Category.find({})
+      .sort({ createdAt: -1 })
+      .skip(page * limit)
+      .limit(parseInt(limit));
+    const count = await Category.find({}).count();
+    return res.status(200).json({ categories, count });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { create_category, update_category } = require("../controllers/category");
+const {
+  create_category,
+  update_category,
+  get_categories,
+  delete_category,
+  get_category,
+} = require("../controllers/category");
 const verifyJWT = require("../middleware/verify-jwt");
 const admin = require("../middleware/admin");
 const router = express.Router();
 
 router.post("/category/create", verifyJWT, admin, create_category);
 router.put("/category/update/:id", verifyJWT, admin, update_category);
+router.delete("/category/delete/:id", verifyJWT, admin, delete_category);
+router.get("/categories", get_categories);
+router.get("/category/:id", get_category);
 
 module.exports = router;
