da8f01aa4ebfed17a5ec689c4ed5bbd092b0b329
Densovan <sarimramata@gmail.com>
2023-04-26 00:35:38 +0700

    finish add to cart

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -49,24 +49,39 @@ exports.get_carts = async (req, res) => {
 exports.update_cart_qty = async (req, res) => {
   const { qty } = req.body;
   try {
-    await Cart.updateOne(
-      // {
-      //   cartItems: { $elemMatch: { _id: req.params.id } },
-      // },
-      {
-        "cartItems._id": req.params.id,
-      },
-      { $set: { "cartItems.$.qty": qty } },
-      {
-        acknowledged: true,
-        insertedId: null,
-        matchedCount: 1,
-        modifiedCount: 1,
-        upsertedCount: 0,
-      }
-    );
-    return res.status(200).json({ msg: "update successfully" });
+    if (qty <= 0) {
+      await Cart.updateOne(
+        {
+          "cartItems._id": req.params.id,
+        },
+        { $pull: { cartItems: { _id: req.params.id } } }
+      );
+      return res.status(200).json({ msg: "update successfully" });
+    } else {
+      await Cart.updateOne(
+        // {
+        //   cartItems: { $elemMatch: { _id: req.params.id } },
+        // },
+        {
+          "cartItems._id": req.params.id,
+        },
+        { $set: { "cartItems.$.qty": qty } },
+        {
+          acknowledged: true,
+          insertedId: null,
+          matchedCount: 1,
+          modifiedCount: 1,
+          upsertedCount: 0,
+        }
+      );
+      return res.status(200).json({ msg: "update successfully" });
+    }
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
 };
+
+// @desc delete cart for users
+// @route DELETE /update_cart
+// @access private/
+// exports.
